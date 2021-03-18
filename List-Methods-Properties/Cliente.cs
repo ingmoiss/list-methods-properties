b0bea533e081465b70aab25eac3a3d0e96813853
Moisés Navarro <moi55navacor@gmail.com>
2021-03-17 22:01:39 -0600

    Added Methods and properties

---
+++ List-Methods-Properties/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_Methods_Properties
+{
+    public class Cliente
+    {
+
+        public Cliente(string nombre, string primerApe, string segundoApe, int edad, string direccion)
+        {
+            this.nombre = nombre;
+            this.primerApe = primerApe;
+            this.segundoApe = segundoApe;
+            this.edad = edad;
+            this.direccion = direccion; 
+        }
+
+        public string nombre { get; set; }
+        public string primerApe { get; set; }
+        public string segundoApe { get; set; }
+        public int edad { get; set; }
+        public string direccion { get; set; }
+
+
+
+    }
+}
--- List-Methods-Properties/Program.cs
+++ List-Methods-Properties/Program.cs
@@ -10,6 +10,25 @@ namespace List_Methods_Properties
     {
         static void Main(string[] args)
         {
+            Cliente oCliente1 = new Cliente("Moises", "Prueba1", "Prueba1", 12, "Prueba1"); 
+            Cliente oCliente2 = new Cliente("Moises2", "Prueba2", "Prueba2", 12, "Prueba2"); 
+            Cliente oCliente3 = new Cliente("Moises3", "Prueba3", "Prueba3", 12, "Prueba3"); 
+            Cliente oCliente4 = new Cliente("Moises4", "Prueba4", "Prueba4", 12, "Prueba4"); 
+            Cliente oCliente5 = new Cliente("Moises5", "Prueba5", "Prueba5", 12, "Prueba5");
+
+            List<Cliente> listCliente = new List<Cliente>
+            {
+                oCliente1, oCliente2, oCliente3, oCliente4, oCliente5
+            };
+
+            listCliente.Add(new Cliente("Moises", "Prueba6", "Prueba6", 12, "Prueba6"));
+
+            foreach(Cliente prueba in listCliente)
+            { 
+                Console.WriteLine(prueba.primerApe + "\n");
+            }
+
+            Console.ReadLine();
         }
     }
 }
